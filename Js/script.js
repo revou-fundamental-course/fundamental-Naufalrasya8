6f5de7b94a204becc0b2c96c687d287a64c38211
naufalrasya8 <naufalrasya3@gmail.com>
2024-07-04 13:40:09 +0700

    Memberikan detail navbar dan menambahkan script teks welcome

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,6 @@
 /* Ini Javascript */
+
+// Function nama
+const namaSpan = document.querySelector('.nama');
+const namaPengguna = prompt('Hallo Selamat Datang, Silahkan masukkan nama kamu : ');
+namaSpan.textContent = namaPengguna;
