26075738f61686b5037458cfdbf223e20b6bc2e5
naufalrasya8 <naufalrasya3@gmail.com>
2024-07-04 17:11:01 +0700

    Menambahkan banner slide dan membuatnya responsive

--- Js/script.js
+++ Js/script.js
@@ -4,3 +4,21 @@
 const namaSpan = document.querySelector('.nama');
 const namaPengguna = prompt('Hallo Selamat Datang, Silahkan masukkan nama kamu : ');
 namaSpan.textContent = namaPengguna;
+
+// Function Slide
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }    
+  slides[slideIndex - 1].style.display = "block";  
+  setTimeout(showSlides, 2000); 
+}
+
+showSlides(); 
